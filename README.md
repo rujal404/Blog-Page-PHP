1a9db4ad0a864fcc091599b6da0f8cc4d8b9a631
Rujal Tamrakar <38482136+rujal404@users.noreply.github.com>
2024-12-20 17:52:05 +0545

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### [PHP & MySQL Blog Web App with Admin Panel]
 
 ![](thumbnail.jpg)
+
+8.17.19 min
--- admin/manage-users.php
+++ admin/manage-users.php
@@ -11,7 +11,7 @@ $users = mysqli_query($connection, $query);
     <!-- ===== Dashboard Section ===== -->
     <section class="dashboard">
 
-    <?php if(isset($_SESSION['add-user-success'])) : ?> // show if add user was successful
+    <?php if(isset($_SESSION['add-user-success'])) :  // show if add user was successful ?>
             <div class="alert__message success container">
                 <p>
                     <?= $_SESSION['add-user-success']; 
@@ -19,19 +19,35 @@ $users = mysqli_query($connection, $query);
                     ?>
                 </p>
             </div>
-    <?php elseif (isset($_SESSION['edit-user-success'])) : ?> // show if edit user was successful
+    <?php elseif (isset($_SESSION['edit-user-success'])) :  // show if edit user was successful ?>
             <div class="alert__message success container">
                 <p>
-                    <?= $_SESSION['edit-user-success']; 
+                    <?= $_SESSION['edit- user-success']; 
                     unset($_SESSION['edit-user-success']);
                     ?>
                 </p>
             </div>
-    <?php elseif (isset($_SESSION['edit-user-success'])) : ?> // show if edit user was not successful
+    <?php elseif (isset($_SESSION['edit-user'])) :  // show if edit user was not successful ?>
             <div class="alert__message error container">
                 <p>
                     <?= $_SESSION['edit-user']; 
                     unset($_SESSION['edit-user']);
+                    ?>
+                </p>
+            </div>
+    <?php elseif (isset($_SESSION['delete-user'])) :  // show if delete user was not successful ?>
+            <div class="alert__message error container">
+                <p>
+                    <?= $_SESSION['delete-user']; 
+                    unset($_SESSION['delete-user']);
+                    ?>
+                </p>
+            </div>
+    <?php elseif (isset($_SESSION['delete-user-success'])) :  // show if delete user was successful ?>
+            <div class="alert__message success container">
+                <p>
+                    <?= $_SESSION['delete-user-success']; 
+                    unset($_SESSION['delete-user-success']);
                     ?>
                 </p>
             </div>
