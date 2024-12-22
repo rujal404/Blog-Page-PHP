82c8447c673731b88284d4fcb029ceb4b8cb863a
Rujal Tamrakar <38482136+rujal404@users.noreply.github.com>
2024-12-22 11:03:01 +0545

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ### [PHP & MySQL Blog Web App with Admin Panel]
 
 ![](thumbnail.jpg)
-
-8.17.19 min
--- admin/add-category.php
+++ admin/add-category.php
@@ -9,10 +9,10 @@ include 'partials/header.php';
         <div class="alert__message error">
             <p>This is an error message</p>
         </div>
-        <form action="">
-            <input type="text" placeholder="Title">
-            <textarea rows="4" placeholder="Description"></textarea>
-            <button type="submit" class="btn">Add Category</button>
+        <form action="<?= ROOT_URL ?>admin/add-category-logic.php" method="POST">
+            <input type="text" name="title" placeholder="Title">
+            <textarea rows="4" name="description" placeholder="Description"></textarea>
+            <button type="submit" name="submit" class="btn">Add Category</button>
         </form>
 
 
