0f0d728a7486be01cd3757054d8aee5db2e5825e
Rujal Tamrakar <38482136+rujal404@users.noreply.github.com>
2024-12-23 17:10:28 +0545

    initial updates

--- admin/add-post-logic.php
+++ admin/add-post-logic.php
@@ -36,27 +36,39 @@ if(isset($_POST['submit'])) {
         if(in_array($extension, $allowed_files)) {
             // make sure image is not too big. (2mb+)
             if($thumbnail['size'] < 2_000_000) {
-
+                // upload thumbnail
+                move_uploaded_file($thumbnail_tmp_name, $thumbnail_destination_path);
             } else {
                 $_SESSION['add-post'] = "File size too big. Should be less than 2mb";
             }
         } else {
             $_SESSION['add-post'] = "File should be png, jpg, or jpeg";
-        }   
-                // upload thumbnail
-         
-        
-        
-            
+        }           
     }
     
     // redirect back (with form data) to add-post page if there is any problem
-   
+    if(isset($_SESSION['add-post'])) {
+        $_SESSION['add-post-data'] = $_POST;
+        header('location: ' . ROOT_URL . 'admin/add-post.php');
+        die();
+    } else {
         // set is_featured of all posts to 0 if is_featured for this post is 1
-        
+        if($is_featured == 1) {
+            $zero_all_is_featured_query = "UPDATE posts SET is_featured=0";
+            $zero_all_is_featured_result = mysqli_query($connection, $zero_all_is_featured_query);
+        }
+    
+    // insert post into database
+    $query = "INSERT INTO posts (title, body, thumbnail, category_id, author_id, is_featured) VALUES ('$title', '$body', '$thumbnail_name', $category_id, $author_id, $is_featured)";
+    $result = mysqli_query($connection, $query);
 
-        // insert post into database
-        
+        if(!mysqli_errno($connection)) {
+            $_SESSION['add-post-success'] = "New post added successfully";
+            header('location: ' . ROOT_URL . 'admin/');
+            die();
+        }
+    }
 }
 
-
+header('loation: ' . ROOT_URL . 'admin/add-post.php');
+die();
